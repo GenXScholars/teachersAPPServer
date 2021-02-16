1458029682e2afbb62787848028d6981aef63c72
ADJAROO OGAGA <ogagaadjaro@gmail.com>
2021-02-16 14:45:56 +0100

    removed deleted files

--- routes/paths/adminPaths.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    "getAllAdmins":"/api/v1/admin",
-    "adminSignUp":"/api/v1/admin/signup",
-    "adminLogin":"/api/v1/admin/login",
-    "getSingleAdmin":"/api/v1/admin/:id",
-    "deletAnAdmin":"/api/v1/admin/:id",
-    "updateAdmin":"admin/v1/admin/:id"
-}
